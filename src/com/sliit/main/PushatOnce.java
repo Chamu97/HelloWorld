70169494d2708ea16cf290ac77359f31192c881e
Chamu97 <majchemachandra@gmail.com>
2019-03-04 10:55:57 +0530

    Second Commit

---
+++ src/com/sliit/main/PushatOnce.java
@@ -0,0 +1,11 @@
+package com.sliit.main;
+
+public class PushatOnce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Try to push at once");
+	}
+
+}
